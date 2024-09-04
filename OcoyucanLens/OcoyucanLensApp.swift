28d004c452e432b61eb956b5cc203bf5439b0ac8
Sofia Zugasti <sofiazugasti04@gmail.com>
2024-09-04 08:39:04 -0600

    Initial Commit

---
+++ OcoyucanLens/OcoyucanLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  OcoyucanLensApp.swift
+//  OcoyucanLens
+//
+//  Created by sofia zugasti on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct OcoyucanLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
